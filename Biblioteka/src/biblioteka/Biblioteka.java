4ef2764187b8494d7dbdad7b33af2ed5a7da2515
bojantomic <tomicb@fon.rs>
2020-03-02 08:55:37 +0100

    Ispravljena greska

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -33,7 +33,7 @@ public class Biblioteka implements BibliotekaInterfejs {
 
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
-		if (autor == null || naslov == null || isbn == null	|| izdavac == null)
+		if (autor == null && naslov == null && isbn == null	&& izdavac == null)
 			return null;		
 		
 		List<Knjiga> rezultat = new LinkedList<Knjiga>();
