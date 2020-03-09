ac67a8f911a58eb27fb8636e7787e33131b4f1a0
bojantomic <tomicb@fon.rs>
2020-03-09 11:10:35 +0100

    Dodata dokumentacija za klasu Biblioteka, dodat doc u gitignore

--- Biblioteka/src/biblioteka/Biblioteka.java
+++ Biblioteka/src/biblioteka/Biblioteka.java
@@ -6,11 +6,33 @@ import java.util.List;
 
 import biblioteka.interfejs.BibliotekaInterfejs;
 
+/**
+ * Klasa predstavlja biblioteku sa knjigama. Knjige se cuvaju u
+ * listi. Klasa implementira interfejs BibliotekaInterfejs.
+ * 
+ * @author Bojan Tomic
+ */
 public class Biblioteka implements BibliotekaInterfejs {
 	
+	/**
+	 * Lista sa knjigama iz biblioteke.
+	 */
 	private List<Knjiga> knjige = new ArrayList<Knjiga>();
 
-	//Dodaje se knjiga na kraj liste
+	/**
+	 * Dodaje novu knjigu u biblioteku. Knjiga se dodaje na kraj
+	 * liste knjiga.
+	 * 
+	 * @param knjiga Nova knjiga koja se dodaje u biblioteku.
+	 * 
+	 * @throws java.lang.RuntimeException Ako je:
+	 * <ul>
+	 * 	<li> uneta knjiga null ili
+	 *  <li> ako vec postoji ista knjiga u biblioteci
+	 * </ul>
+	 * 
+	 * @see <a href = "http://ai.fon.bg.ac.rs/">AI SAJT</a>
+	 */
 	@Override
 	public void dodajKnjigu(Knjiga knjiga) {
 		if (knjiga == null)
@@ -22,6 +44,13 @@ public class Biblioteka implements BibliotekaInterfejs {
 		knjige.add(knjiga);
 	}
 
+	/**
+	 * Brise knjigu iz liste biblioteke.
+	 * 
+	 * @param knjiga Knjiga koju je potrebno obrisati.
+	 * 
+	 * @throws java.lang.RuntimeException Ako je uneta knjiga null.
+	 */
 	@Override
 	public void obrisiKnjigu(Knjiga knjiga) {
 		if (knjiga == null)
@@ -30,11 +59,31 @@ public class Biblioteka implements BibliotekaInterfejs {
 		knjige.remove(knjiga);
 	}
 
+	/**
+	 * Vraca listu sa svim knjigama iz biblioteke.
+	 * 
+	 * @return Lista svih knjiga.
+	 */
 	@Override
 	public List<Knjiga> vratiSveKnjige() {
 		return knjige;
 	}
 
+	/**
+	 * Pretrazuje biblioteku po zadatim parametrima i vraca
+	 * listu knjiga koje odgovaraju pretrazi. Ako se neki
+	 * parametar izostavi (prosledi se null), pretrazivanje
+	 * se vrsi po ostalim parametrima.
+	 * 
+	 * @param autor Autor knjige kao kriterijum
+	 * @param isbn	ISBN knjige kao kriterijum
+	 * @param naslov Naslov knjige kao kriterijum (moze
+	 *  i samo deo naslova)
+	 * @param izdavac Izdavac kao kriterijum
+	 * 
+	 * @return Lista knjiga koje odgovaraju kriterijumima ili null
+	 * ako su svi kriterijumi pretrage uneto kao null.
+	 */
 	@Override
 	public List<Knjiga> pronadjiKnjigu(Autor autor, String isbn, String naslov, String izdavac) {
 		if (autor == null && naslov == null && isbn == null	&& izdavac == null)
